b5fa0ca1550976a33c17afd47cbf9868f40a6689
Elpar <37876715+Elpar@users.noreply.github.com>
2019-01-18 12:58:03 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/HW333.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Asafr/Desktop/SharedWithUbuntu/Compilation/HW333/Function.cpp" "C:/Users/Asafr/Desktop/SharedWithUbuntu/Compilation/HW333/cmake-build-debug/CMakeFiles/HW333.dir/Function.cpp.obj"
+  "C:/Users/Asafr/Desktop/SharedWithUbuntu/Compilation/HW333/Quads.cpp" "C:/Users/Asafr/Desktop/SharedWithUbuntu/Compilation/HW333/cmake-build-debug/CMakeFiles/HW333.dir/Quads.cpp.obj"
+  "C:/Users/Asafr/Desktop/SharedWithUbuntu/Compilation/HW333/Symbol.cpp" "C:/Users/Asafr/Desktop/SharedWithUbuntu/Compilation/HW333/cmake-build-debug/CMakeFiles/HW333.dir/Symbol.cpp.obj"
+  "C:/Users/Asafr/Desktop/SharedWithUbuntu/Compilation/HW333/main.cpp" "C:/Users/Asafr/Desktop/SharedWithUbuntu/Compilation/HW333/cmake-build-debug/CMakeFiles/HW333.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/HW333.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW333.dir/main.cpp.obj"
+  "CMakeFiles/HW333.dir/Quads.cpp.obj"
+  "CMakeFiles/HW333.dir/Symbol.cpp.obj"
+  "CMakeFiles/HW333.dir/Function.cpp.obj"
+  "HW333.pdb"
+  "HW333.exe"
+  "HW333.exe.manifest"
+  "HW333.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW333.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
